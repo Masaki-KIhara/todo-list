4f9859b303ee909c235c4b2cd1eeb2a72938bce3
Masaki Kihara <masaki1224.1998@gmail.com>
2023-08-08 23:08:55 +0900

    tailwindインストール

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <p>aaa</p>;
+  return (
+    <button className="bg-red-500 text-white font-bold py-2 px-4 ">
+      Hello Tailwind
+    </button>
+  );
 }
 
 export default App;

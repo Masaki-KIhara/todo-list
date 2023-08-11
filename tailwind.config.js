094ae78079b47715c52099bc43ca3905e144c879
Masaki Kihara <masaki1224.1998@gmail.com>
2023-08-11 14:36:19 +0900

    color設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    colors: { gray: "#9ca3af", white: "#f3f4f6", lightGray: "#a3a3a3" },
   },
   plugins: [],
 };

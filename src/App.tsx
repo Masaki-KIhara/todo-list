a98409395a66371cf806e77a76bc978ed56d1a5b
Masaki Kihara <masaki1224.1998@gmail.com>
2023-08-17 22:22:15 +0900

    一括削除機能実装

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,24 @@ import { ChangeEvent, useEffect, useState } from "react";
 type TableBodyType = {
   date: string;
   content: string;
+  checked: boolean;
+};
+
+type CheckTabeleType = {
+  val: ChangeEvent<HTMLInputElement>;
+  key: number;
 };
 
 function App() {
-  const tableHeaderList = ["選択", "登録日", "TODO", "削除"];
+  const tableHeaderList = ["登録日", "TODO", "削除"];
   const [inputValue, setInputValue] = useState<string>("");
   const [tableBody, setTableBody] = useState<TableBodyType>({
     date: "",
     content: "",
+    checked: false,
   });
   const [tableBodyList, setTableBodyList] = useState<TableBodyType[]>([]);
-
+  const [checkedList, setCheckedList] = useState<Array<number>>([]);
   const onChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
   };
@@ -27,12 +34,64 @@ function App() {
     setTableBodyList((prev) => [...prev]);
   };
 
+  const deleteAllTable = () => {
+    if (window.confirm("選択されているTODOを削除しますか？")) {
+      checkedList.reverse().forEach((item) => {
+        tableBodyList.splice(item, 1);
+        setCheckedList([]);
+        setTableBodyList((prev) => [...prev]);
+      });
+    }
+  };
+
+  //オブジェクトのcheckedを変更する
+  const checkedTable = ({ val, key }: CheckTabeleType) => {
+    const upDateList = [...tableBodyList];
+    upDateList[key].checked = val.target.checked;
+    setTableBodyList(upDateList);
+    if (val.target.checked === true) {
+      checkedList.push(key);
+      setCheckedList(checkedList);
+    } else if (val.target.checked === false) {
+      checkedList.forEach((item, index) => {
+        if (index === key) {
+          checkedList.splice(index, 1);
+          setCheckedList(checkedList);
+        }
+      });
+    }
+  };
+
+  const checkAll = (val: ChangeEvent<HTMLInputElement>) => {
+    setTableBodyList(
+      tableBodyList.map((item) => ({
+        ...item,
+        checked: val.target.checked,
+      }))
+    );
+    if (val.target.checked) {
+      for (let count = 0; count < tableBodyList.length; count++) {
+        checkedList.push(count);
+        setCheckedList(checkedList);
+      }
+    } else if (!val.target.checked) {
+      for (let count = 0; count < tableBodyList.length; count++) {
+        checkedList.splice(count, 1);
+        setCheckedList(checkedList);
+      }
+    }
+  };
+
   useEffect(() => {
     const now = new Date();
     const refistrationDay = `${now.getFullYear()}/${
       now.getMonth() + 1
     }/${now.getDate()}`;
-    setTableBody({ date: refistrationDay, content: inputValue });
+    setTableBody({
+      date: refistrationDay,
+      content: inputValue,
+      checked: false,
+    });
   }, [inputValue]);
 
   return (
@@ -61,48 +120,64 @@ function App() {
         {tableBodyList.length === 0 ? (
           <p>登録されているTODOはありません。</p>
         ) : (
-          <table>
-            <thead>
-              <tr>
-                {tableHeaderList.map((item, key) => {
+          <div>
+            <button onClick={deleteAllTable}>一括削除</button>
+            <table>
+              <thead>
+                <tr>
+                  <th className="text-center max-w-[440px] min-w-[120px] border-[1px] border-gray-200">
+                    <input type="checkbox" onChange={checkAll} />
+                    選択
+                  </th>
+                  {tableHeaderList.map((item, key) => {
+                    return (
+                      <th
+                        key={key}
+                        className="bg-gray-100 px-3 py-1 border-[1px] border-gray-200"
+                      >
+                        {item}
+                      </th>
+                    );
+                  })}
+                </tr>
+              </thead>
+              <tbody>
+                {tableBodyList.map((item, key) => {
                   return (
-                    <th
-                      key={key}
-                      className="bg-gray-100 px-3 py-1 border-[1px] border-gray-200"
-                    >
-                      {item}
-                    </th>
+                    <tr key={key}>
+                      <td className="text-center max-w-[440px] min-w-[120px] border-[1px] border-gray-200">
+                        <input
+                          type="checkbox"
+                          onChange={(val) => {
+                            checkedTable({
+                              val: val,
+                              key: key,
+                            });
+                          }}
+                          checked={tableBodyList[key].checked}
+                        />
+                      </td>
+                      <td className="max-w-[440px] px-[10px] min-w-[120px] border-[1px] border-gray-200 text-center break-all">
+                        {item.date}
+                      </td>
+                      <td className="max-w-[440px] px-[10px] min-w-[120px] border-[1px] border-gray-200 text-center break-all">
+                        {item.content}
+                      </td>
+                      <td className="max-w-[440px] px-[10px] min-w-[120px] border-[1px] border-gray-200 text-center break-all">
+                        <button
+                          onClick={() => {
+                            deleteTable(key);
+                          }}
+                        >
+                          削除
+                        </button>
+                      </td>
+                    </tr>
                   );
                 })}
-              </tr>
-            </thead>
-            <tbody>
-              {tableBodyList.map((item, key) => {
-                return (
-                  <tr key={key}>
-                    <td className="text-center max-w-[440px] min-w-[120px] border-[1px] border-gray-200">
-                      <input type="checkbox" />
-                    </td>
-                    <td className="max-w-[440px] px-[10px] min-w-[120px] border-[1px] border-gray-200 text-center break-all">
-                      {item.date}
-                    </td>
-                    <td className="max-w-[440px] px-[10px] min-w-[120px] border-[1px] border-gray-200 text-center break-all">
-                      {item.content}
-                    </td>
-                    <td className="max-w-[440px] px-[10px] min-w-[120px] border-[1px] border-gray-200 text-center break-all">
-                      <button
-                        onClick={() => {
-                          deleteTable(key);
-                        }}
-                      >
-                        削除
-                      </button>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
+              </tbody>
+            </table>
+          </div>
         )}
       </div>
     </>

45ab0cd7e0838f87f30d5ed38e888c60561c1660
Masaki Kihara <masaki1224.1998@gmail.com>
2023-08-19 10:54:00 +0900

    不要な処理削除

--- src/hooks/useTodo.tsx
+++ src/hooks/useTodo.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useEffect, useState } from "react";
+import { ChangeEvent, useState } from "react";
 import { CheckTabeleType, TableBodyType } from "../types/todotype";
 
 export const useTodo = () => {
@@ -9,14 +9,23 @@ export const useTodo = () => {
     checked: false,
   });
   const [tableBodyList, setTableBodyList] = useState<TableBodyType[]>([]);
-  const [checkedList, setCheckedList] = useState<Array<number>>([]);
   const [checkAllState, setCheckedAllState] = useState<boolean>(false);
+
   const onChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
+    const now = new Date();
+    const refistrationDay = `${now.getFullYear()}/${
+      now.getMonth() + 1
+    }/${now.getDate()}`;
+    setTableBody({
+      date: refistrationDay,
+      content: e.target.value,
+      checked: false,
+    });
   };
 
   const addTable = () => {
-    setTableBodyList((prev) => [...prev, tableBody]);
+    setTableBodyList((prev) => [...prev, { ...tableBody }]);
   };
 
   const deleteTable = (key: number) => {
@@ -27,28 +36,15 @@ export const useTodo = () => {
   const deleteAllTable = () => {
     if (window.confirm("選択されているTODOを削除しますか？")) {
       setTableBodyList(tableBodyList.filter((item) => item.checked === false));
-      setCheckedList([]);
       setCheckedAllState(false);
     }
   };
 
-  //オブジェクトのcheckedを変更する
   const checkedTable = ({ val, key }: CheckTabeleType) => {
     const upDateList = [...tableBodyList];
     upDateList[key].checked = val.target.checked;
     setTableBodyList(upDateList);
     setCheckedAllState(false);
-    if (val.target.checked === true) {
-      checkedList.push(key);
-      setCheckedList(checkedList);
-    } else if (val.target.checked === false) {
-      checkedList.forEach((item, index) => {
-        if (index === key) {
-          checkedList.splice(index, 1);
-          setCheckedList(checkedList);
-        }
-      });
-    }
   };
 
   const checkAll = (val: ChangeEvent<HTMLInputElement>) => {
@@ -59,36 +55,12 @@ export const useTodo = () => {
         checked: val.target.checked,
       }))
     );
-    if (val.target.checked) {
-      for (let count = 0; count < tableBodyList.length; count++) {
-        checkedList.push(count);
-        setCheckedList(checkedList);
-      }
-    } else if (!val.target.checked) {
-      for (let count = 0; count < tableBodyList.length; count++) {
-        checkedList.splice(count, 1);
-        setCheckedList(checkedList);
-      }
-    }
   };
-
-  useEffect(() => {
-    const now = new Date();
-    const refistrationDay = `${now.getFullYear()}/${
-      now.getMonth() + 1
-    }/${now.getDate()}`;
-    setTableBody({
-      date: refistrationDay,
-      content: inputValue,
-      checked: false,
-    });
-  }, [inputValue]);
 
   return {
     tableBodyList,
     inputValue,
     checkAllState,
-    checkedList,
     onChangeInput,
     addTable,
     deleteTable,
